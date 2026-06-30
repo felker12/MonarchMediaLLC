a02bd2b25ae5c3e74ddc37729fb3ad20fd77fee0
felker12 <kanthonyfelker@yahoo.com>
2026-06-30 09:38:40 -0400

    added css for the dash, projects, and team pages

--- MonarchMediaLLC.ApiService/Program.cs
+++ MonarchMediaLLC.ApiService/Program.cs
@@ -1,7 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using MonarchMediaLLC.ApiService.Data;
 using MonarchMediaLLC.Shared;
-using System.Diagnostics;
 
 var builder = WebApplication.CreateBuilder(args);
 
